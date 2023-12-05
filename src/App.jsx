0a70894aaee18254052cdac61dd0335541f45d21
AmalStudy <amalengstudy@gmail.com>
2023-12-05 01:42:32 -0800

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-
-// Your existing imports
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 
-const API_KEY = "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";
+const API_KEY = "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";
 
-// System message definition remains the same
-const systemMessage = {
-  "role": "system",
-  "content": "Explain things like you're talking to a software professional with 2 years of experience."
-};
+const footballKeywords = ['football', 'soccer', 'FIFA', 'UEFA', 'goal', 'World Cup', 'player', 'match', 'league', 'team'];
+
+function isFootballQuestion(message) {
+  return footballKeywords.some(keyword => message.toLowerCase().includes(keyword));
+}
 
 function ChatButton({ onClick }) {
   return (
@@ -18,7 +16,7 @@ function ChatButton({ onClick }) {
   );
 }
 
-function ChatInterface({ onClose, onClearChat, messages, handleSend, isTyping }) {
+function ChatInterface({ onClose, onClearChat, messages, handleSend, isTyping, handleAnswerLengthChange, answerLength }) {
   return (
     <div className="chat-container">
       <div className="message-list">
@@ -39,6 +37,28 @@ function ChatInterface({ onClose, onClearChat, messages, handleSend, isTyping })
           }
         }} 
       />
+      <div className="answer-length-controls">
+        <label style={{color:'black'}}>
+          <input 
+            type="radio" 
+            name="answerLength" 
+            value="short"
+            checked={answerLength === 'short'}
+            onChange={handleAnswerLengthChange} 
+          />
+          Short Answer
+        </label>
+        <label style={{color:'black'}}>
+          <input 
+            type="radio" 
+            name="answerLength" 
+            value="full"
+            checked={answerLength === 'full'}
+            onChange={handleAnswerLengthChange} 
+          />
+          Full Answer
+        </label>
+      </div>
       <button className="close-chat-button" onClick={onClose}>Close Chat</button>
       <button className="clear-chat-button" onClick={onClearChat}>Clear Chat</button>
     </div>
@@ -49,29 +69,40 @@ function App() {
   const [showChat, setShowChat] = useState(false);
   const [messages, setMessages] = useState([
     {
-      message: "Hello! I am your assistant, I work based on the chat-GPT API. Ask me any question",
+      message: "Hello! I am your assistant, I work based on the chat-GPT API. Ask me any football-related question",
       sentTime: "just now",
       sender: "ChatGPT"
     }
   ]);
   const [isTyping, setIsTyping] = useState(false);
+  const [answerLength, setAnswerLength] = useState('full'); // State for answer length preference
 
   const handleChatToggle = () => {
     setShowChat(!showChat);
   };
 
+  const handleAnswerLengthChange = (e) => {
+    setAnswerLength(e.target.value);
+  };
+
   const handleSend = async (message) => {
+    if (!isFootballQuestion(message)) {
+      setMessages([...messages, {
+        message: "I can only answer football-related questions. Please ask a question about football.",
+        sender: "system"
+      }]);
+      return;
+    }
+
     const newMessage = {
       message,
-      direction: 'outgoing',
       sender: "user"
     };
 
-    const newMessages = [...messages, newMessage];
-    setMessages(newMessages);
+    setMessages([...messages, newMessage]);
 
     setIsTyping(true);
-    await processMessageToChatGPT(newMessages);
+    await processMessageToChatGPT(messages.concat(newMessage));
   };
 
   const clearChat = () => {
@@ -80,42 +111,55 @@ function App() {
       sentTime: "just now",
       sender: "system"
     }]);
-    // Optionally, add setShowChat(false) and setIsTyping(false) if needed
   };
 
   async function processMessageToChatGPT(chatMessages) {
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if (messageObject.sender === "ChatGPT") {
-        role = "assistant";
-      } else {
-        role = "user";
-      }
-      return { role: role, content: messageObject.message }
-    });
-
+    const apiMessages = chatMessages.map((messageObject) => ({
+      role: messageObject.sender === "ChatGPT" ? "assistant" : "user",
+      content: messageObject.message
+    }));
+  
     const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [systemMessage, ...apiMessages]
-    }
-
-    await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + API_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
+      model: "gpt-3.5-turbo",
+      messages: apiMessages
+    };
+  
+    try {
+      const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${API_KEY}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(apiRequestBody)
+      });
+  
+      if (!response.ok) {
+        const errorDetails = await response.text();
+        throw new Error(`HTTP error! status: ${response.status}, details: ${errorDetails}`);
+      }
+  
+      const data = await response.json();
+  
+      if (data.choices && data.choices.length > 0) {
+        setMessages([...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT"
+        }]);
+      } else {
+        console.error('No valid response received from API');
+      }
+    } catch (error) {
+      console.error('Fetch error:', error.message);
       setMessages([...chatMessages, {
-        message: data.choices[0].message.content,
-        sender: "ChatGPT"
+        message: "An error occurred while processing your request.",
+        sender: "system"
       }]);
+    } finally {
       setIsTyping(false);
-    });
+    }
   }
+  
 
   return (
     <div className="App">
@@ -127,6 +171,8 @@ function App() {
           messages={messages}
           handleSend={handleSend}
           isTyping={isTyping}
+          handleAnswerLengthChange={handleAnswerLengthChange}
+          answerLength={answerLength}
         />
       )}
     </div>
